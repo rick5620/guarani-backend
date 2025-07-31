a5f4b712c0290c59f876fd2807a42963ead9d9a2
Henrique Gonçalves <henriquesg1717@gmail.com>
2025-07-31 15:16:46 -0300

    Usar PORT do Render

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const routes = require("./routes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
