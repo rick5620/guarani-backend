7213d4597411e882124d3a78e44af39b15a39b5a
Henrique Gonçalves <henriquesg1717@gmail.com>
2025-08-05 07:30:20 -0300

    Atualiza server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ app.use(cors({origin:['https://guaranimoveis.netlify.app']}));
 app.use(express.json());
 
 // Pasta pública para uploads de imagens
-app.use("/imagens", express.static(path.join(__dirname, "../frontend/imagens")));
+app.use(express.static(path.join(__dirname, "../frontend")));
+app.use("/imagens",express.static(path.join(__dirname,"../frontend/imagens")));
 
 // Rotas da API
 app.use("/api", routes);
